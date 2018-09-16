db74e411293b5cc85794695605c330534b06823d
System Administrator <root@tealdeMacBook-Pro.local>
2018-09-16 17:38:17 +0800

    init
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up-to-date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   package-lock.json
    #	modified:   package.json
    #

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 const Koa = require('koa');
 const app = module.exports = new Koa();
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser')
 const router = new Router();
 const mainCtl = require('./lib/main-router');
 
@@ -16,6 +17,7 @@ app.on('error', (err, cxt) => {
     console.log('Caught exception: ', err, cxt);
 });
 
-router.post('/', mainCtl.handerPost);
+router.post('/', mainCtl.handlerPost);
 
+app.use(bodyParser())
 app.use(router.routes()).use(router.allowedMethods());
